17de8315b77c461c2faa43c97911e075a95419ed
Espen Bakken <bakken.espen@gmail.com>
2020-09-24 11:40:36 +0200

    TWOS green, fix maven build

--- src/main/java/no/kristiania/yatzy/YatzyGame.java
+++ src/main/java/no/kristiania/yatzy/YatzyGame.java
@@ -4,9 +4,10 @@ public class YatzyGame {
     public int score(YatzyCategory category, int [] dice) {
         var result = 0;
         for (int die : dice) {
-            if(die == 1) {
-                result += 1;
-
+            if (die == 1 && category == YatzyCategory.ONES) {
+                result += die;
+            } else if (die == 2 && category == YatzyCategory.TWOS){
+                result += die;
             }
         }
         return result;
